46dc49793724e8e675b9f7cfb47712c9132c459a
bila <maksy3it@gmail.com>
2024-10-09 12:13:59 -0400

    terraform w10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-na-terraform" #replace with your bucket
+    bucket         = "jhk-terraform" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable"
+    dynamodb_table = "hgkjg"
   }
 }
